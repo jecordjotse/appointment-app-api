3996fe2fba110f60efe4826cf2551e7ebc1bf722
Jerome Cordjotse <jeromecordjotse@gmail.com>
2022-04-25 18:37:12 +0000

    Fixes (#6)
    * add node version
    
    * fix port issue
    
    * nn

--- db-prod.js
+++ db-prod.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://dbUser:xOSemiRQPKWCgYry@cluster0.lisho.mongodb.net/appointmentDB?authSource=admin&replicaSet=atlas-ffxj3i-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
-  (err) => {
-    if (!err) console.log("MongoDB connection succeeded.");
-    else
-      console.log(
-        "Error in DB connection: ",
-        JSON.stringify(err, undefined, 2)
-      );
-  }
-);
+mongoose.connect("", (err) => {
+  if (!err) console.log("MongoDB connection succeeded.");
+  else
+    console.log("Error in DB connection: ", JSON.stringify(err, undefined, 2));
+});
 
 module.export = mongoose;
