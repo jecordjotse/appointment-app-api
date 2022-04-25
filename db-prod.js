0e422627cb6d33ee2771199c404d33898b98b707
Jerome Cordjotse <jeromecordjotse@gmail.com>
2022-04-25 19:54:09 +0000

    db auth (#7)
    * db setup
    
    * fixes
    
    * change app name
    
    * add node version (#3)
    
    * Fixes (#4)
    
    * add node version
    
    * fix port issue
    
    * Fixes (#6)
    
    * add node version
    
    * fix port issue
    
    * nn
    
    * db auth

--- db-prod.js
+++ db-prod.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-
+const user = process.env.user_DB;
+const pass = process.env.pass;
 mongoose.connect(
-  "mongodb+srv://dbUser:xOSemiRQPKWCgYry@cluster0.lisho.mongodb.net/appointmentDB?authSource=admin&replicaSet=atlas-ffxj3i-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+  `mongodb+srv://${user}:${pass}@cluster0.lisho.mongodb.net/appointmentDB?authSource=admin&replicaSet=atlas-ffxj3i-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
   (err) => {
     if (!err) console.log("MongoDB connection succeeded.");
     else
